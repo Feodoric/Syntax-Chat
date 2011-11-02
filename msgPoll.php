73711b9153fe913c1fea0748fbd906ed0a790050
Russell Stringer <r.stringer@gmail.com>
2011-11-02 17:03:06 -0500

    first pass at select query to grab new messages

--- msgPoll.php
+++ msgPoll.php
@@ -4,7 +4,21 @@ $m = new Mongo();
 $db = $m->chatServer;
 $collection = $db->chats;
 
+$chatId = $_POST['chatId'];
+$ts = new MongoDate(strtotime($_POST['lastReceive']));
 
+$res = $collection->find(array(
+	'_id' => $chatId, 
+	'message.timestamp' => array('$gt' => $ts),
+	'message.username' => array('$not' => $_POST['username'])
+));
+
+$return = array(
+	'timestamp' => date('Y-m-d h:i:s'),
+	'messages' => $res
+);
+
+echo json_encode($return);
 
 /* if (rand(0,2) > 1){ */
 /* 	echo json_encode(array( */
